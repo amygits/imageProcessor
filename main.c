0a65ba491d7dfffa019008fdf71f2885af6a54ed
Amy Ma <amyma402@gmail.com>
2021-09-01 03:59:22 -0700

    Update main file to have getops

--- main.c
+++ main.c
@@ -11,82 +11,132 @@
 
 #include "BmpProcessor.h"
 #include "PpmProcessor.h"
-
+#include <unistd.h>
+#include <getopt.h>
 
 
 int main(int argc, char** argv) {
     
-    struct Pixel* pixels = init_Pixel();
+    char* inputfile;
+    char* outputfile;
+    char* type;
     
-    //Load image files to make copy of
-    printf("\n///////////////////////////////////////////\n");
-    //FILE* bmpInput1 = fopen("test2.bmp", "rb");
-    FILE* ppmInput1 = fopen("nehoymenoy.ppm", "r");
+    int opt;
     
-    //struct BMP_Header* header_bmp = init_BMP_Header();
-    //struct DIB_Header* header_dib = init_DIB_Header();
-    //struct PPM_Header* header_ppm = init_PPM_Header();
+    int rvalue = 0;
+    int gvalue = 0;
+    int bvalue = 0;
     
-    //struct PPM_Header* temp;
-    struct BMP_Header* bmpTemp;
-    //struct DIB_Header* dibTemp;
-    printf("signature: %c%c\n", bmpTemp->signature[0], bmpTemp->signature[1]);
-    printf("size: %d\n", bmpTemp->size);
-    printf("reserved1: %d\n", bmpTemp->reserved1);
-    printf("reserved2: %d\n", bmpTemp->reserved2);
-    printf("offset: %d\n", bmpTemp->offset_pixel_array);
+    int oFlag = 0;
+    int rFlag = 0;
+    int gFlag = 0;
+    int bFlag = 0;
+    int tFlag = 0;
     
-    //makePPMHeader(temp, 100, 100);
-    makeBMPHeader(bmpTemp, 100, 100);
-    printf("size: %d\n", bmpTemp->size);
-    printf("reserved1: %d\n", bmpTemp->reserved1);
-    printf("reserved2: %d\n", bmpTemp->reserved2);
-    printf("offset: %d\n", bmpTemp->offset_pixel_array);
+        while((opt = getopt(argc, argv, ":i:o:r:g:b:t:")) != -1){
+        switch(opt) {
+            case 'i':
+                inputfile = optarg;
+                break;
+            case ':':
+                printf("Please enter a file name\n");
+                break;
+            case 'o':
+                oFlag = 1;
+                outputfile = optarg;
+                //printf("output filename: %s\n", outputfile);
+                break;
+            case 'r':
+                rFlag = 1;
+                rvalue = atoi(optarg);
+                //printf("r shift: %d", rvalue);
+                break;
+            case 'g':
+                gFlag = 1;
+                gvalue = atoi(optarg);
+                //printf("g shift: %d", gvalue);
+                break;
+            case 'b':
+                bFlag = 1;
+                bvalue = atoi(optarg);
+                //printf("b shift: %d", bvalue);
+                break;
+            case 't':
+                tFlag = 1;
+                type = optarg;
+                //printf("type: %s\n", type);
+                break;
+            case '?':
+                printf("Unknown option: %c\n", optopt);
+                break;
+        }
+    }
     
-    //readBMPHeader(bmpInput1, header_bmp);
-    //readDIBHeader(bmpInput1, header_dib);
-    //int width = header_dib->width;
-    //int height = header_dib->height;
+    printf("filename: %s\n", inputfile);
+    printf("oFlag = %d, rFlag = %d, gFlag = %d, bFlag = %d, tFlag = %d\n", oFlag, rFlag, gFlag, bFlag, tFlag);
+   printf("Values:\nrshift = %d, gshift = %d, bshift = %d\n", rvalue, gvalue, bvalue);
+    for (; optind < argc; optind++){
+        printf("Given extra arguments: %s\n", argv[optind]);
+    }
     
-    //readPPMHeader(ppmInput1, temp);
-    //int width = header_ppm->width;
-    //int height = header_ppm->height;   
-    //printf("width: %d\nheight: %d\n", width, height);
-    //readPixelsPPM(ppmInput1, pixels, width, height);
-    
-    printf("\n///////////////////////////////////////////\n");
-    
-    // Manipulate the colors on binary level
-    /*int r, g, b;
-    r = 10;
-    g = 11;
-    b = -55;
-    colorShiftPixels(pixels, width, height, r, g, b);*/
-   
-    // Write to files (Save copy)
-    //printf("Preparing to write to file....\n");
-    //FILE* bmpOutput1 = fopen("output1.bmp", "wb"); //write binary
-    //FILE* bmpOutput2 = fopen("output2.bmp", "wb"); //write binary
-    FILE* ppmOutput1 = fopen("output1.ppm", "w"); 
-    //writeBMPHeader(bmpOutput1, header_bmp);
-    //writeDIBHeader(bmpOutput1, header_dib);
-    //writePixelsBMP(bmpOutput1, pixels, width, height);
-    
-    //fclose(bmpInput1);
-    //fclose(bmpOutput1);
-    
-    printf("\n///////////////////////////////////////////\n");
-    
-    //writePPMHeader(ppmOutput1, header_ppm);
-    //writePixelsPPM(ppmOutput1, pixels, width, height);
-    
-    //close files
-    fclose(ppmInput1);
-    fclose(ppmOutput1);    
-    
-    free(pixels);
-    //free(header_bmp);
-    //free(header_dib);
+    int dflen;
+    // If inputfile is valid
+    if(inputfile != NULL){
+        dflen = strlen(inputfile);
+        if (dflen >= 5 
+                && ((strcmp(&inputfile[dflen - 4], ".bmp") == 0) ||
+                (strcmp(&inputfile[dflen - 4], ".ppm") == 0))
+                && (access(inputfile, F_OK) != -1)) {
+            printf("Opening file %s\n", inputfile);
+            FILE* fileImport = fopen(inputfile, "r");
+            struct Pixel* pixelBody = init_Pixel();
+            int height = 0;
+            int width = 0;
+            
+            //check to see what type of file it is (ppm/bmp) before performing read/write actions
+            
+            // if bmp, performs read actions
+            if (strcmp(&inputfile[dflen - 4], ".bmp") == 0) {
+                struct BMP_Header* bmpheader = init_BMP_Header();
+                struct DIB_Header* dibheader = init_DIB_Header();
+                readBMPHeader(inputfile, bmpheader);
+                readDIBHeader(inputfile, dibheader);
+                width = dibheader->width;
+                height = dibheader->height;
+                readPixelsBMP(inputfile, pixelBody, width, height);
+            } 
+            //if ppm, performs read actions
+            if (strcmp(&inputfile[dflen - 4], ".ppm") == 0) {
+                struct PPM_Header* ppmheader = init_PPM_Header();
+                readPPMHeader(inputfile, ppmheader);
+                width = ppmheader->width;
+                height = ppmheader->height;
+                readPixelsPPM(inputfile, pixelBody, width, height);
+            }
+            // if any of the r/g/b options are flagged
+            if (rFlag == 1 || bFlag == 1 || gFlag == 1) {
+                colorShiftPixels(pixelBody, width, height, rvalue, gvalue, bvalue);
+            }
+            // if output file name is flagged
+            if (oFlag == 1){
+                FILE* fileExport = fopen(outputfile, "w");
+                //if type == PPM
+                
+                //else export as BMP
+            }
+            //
+            printf("closing file %s\n", inputfile);
+            fclose(fileImport);
+        } else {
+            printf("Data file has an invalid name or does not exist\n");
+            //print_usage();
+            exit(1);
+        }
+    } else { 
+        printf("No data file name provided.  This is a required field\n");
+        //print_usage();
+        exit(1);
+    }
     
     return (EXIT_SUCCESS);
 }
